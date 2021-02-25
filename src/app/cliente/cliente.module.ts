2d66a331729264ca395bfdb14bac0a53dbf76ce5
SAULOAM <saul.osk@hotmail.com>
2021-02-24 21:44:09 -0300

    Projeto de referencia completo

--- src/app/cliente/cliente.module.ts
+++ src/app/cliente/cliente.module.ts
@@ -1,20 +1,35 @@
-import { NgModule, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { FileUploadModule } from 'primeng/fileupload';
+import { InputMaskModule } from 'primeng/inputmask';
+import { DropdownModule } from 'primeng/dropdown';
 import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { ClienteRoutingModule } from './cliente-routing.module';
-import { FormulalioComponent } from './formulalio/formulalio.component';
-import {  TabelaComponent } from './tabela/tabela.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { TabelaComponent } from './tabela/tabela.component';
 
 @NgModule({
   declarations: [
-    FormulalioComponent,
+    FormularioComponent,
     TabelaComponent
   ],
   imports: [
     CommonModule,
+    BrowserAnimationsModule,
     ClienteRoutingModule,
-    TableModule
+    FormsModule,
+    DropdownModule,    
+    InputMaskModule,
+    FileUploadModule,
+    TableModule, 
+    ButtonModule, 
+    InputTextModule
   ]
 })
 export class ClienteModule { }
